d7c864aae854d7a55860e90b914f69d3db74543a
amishcodetrade <amish.kajavadra@mail.codetrade.io>
2023-01-11 11:24:51 +0530

    Initial commit

---
+++ lib/ans.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String answerText;
+  final Color answerColor;
+  final Function answertap;
+  Answer({this.answerText,this.answerColor,this.answertap});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap:answertap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 5,horizontal: 20),
+        width:  MediaQuery.of(context).size.width,
+        height: 50,
+        child: Center(child: Text(answerText,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),)),
+        color: answerColor,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:quize/main.dart';
-import 'package:quize/second.dart';
+import 'package:quize/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,103 +16,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home:  MyHomePage(title: 'Fquize'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body:Column(
-        children: [
-          Center(child: Text("what's your favorite color?",style: TextStyle(fontSize: 25,color: Colors.black,fontWeight: FontWeight.bold),),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10, right: 10),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50)),
-              onPressed: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => Seconf()),
-                );
-              },
-              child: Text('Black',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10, right: 10),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50)),
-              onPressed: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => Seconf()),
-                );
-              },
-              child: Text('Red',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10, right: 10),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50)),
-              onPressed: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => Seconf()),
-                );
-              },
-              child: Text('Green',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10, right: 10),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50)),
-              onPressed: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => Seconf()),
-                );
-              },
-              child: Text('White',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-            ),
-          ),
-        ],
-      ),
+      home: MyHomePage(),
+      // routes: {
+      //   '/result' :(context) => Result(),
+      //
+      // },
     );
   }
 }
